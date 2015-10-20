b8aac6ec85f8f935dfedc687e51eefdf7fce9be9
Ryan Cole <ryan@askdesigners.eu>
2015-10-20 11:19:40 +0200

    scrolling loading

--- contents/js/pages.js
+++ contents/js/pages.js
@@ -21,27 +21,46 @@ jQuery(document).ready(function($) {
         };
         return function () {
             // console.log('visibility ' + isElementInViewport($('#redakci-techniku')));
-            console.log('handle');
-            for (var i = articlesArr.length - 1; i >= 0; i--) {
-                // console.log('visibility of ' + articlesArr[i].id + ' : ' + isElementInViewport(articlesArr[i].jq));
-                if(isElementInViewport(articlesArr[i].jq) && !articlesArr[i].isLoaded){
-                    console.log('loading');
-                    articlesArr[i].isLoaded = true;
-                    articlesArr[i].jq.load( articlesArr[i].link + " .articleContents" );
+            // console.log('handle');
+            
+            var loader = function(elem){
+                console.log('loading', elem);
+                elem.jq.find('.articleTarget').fadeOut('slow', function() {
+                    elem.jq.find('.articleTarget').load( elem.link + " .articleContents", function(e){
+                        elem.isLoaded = true;
+                        console.log('loaded!', elem);
+                        elem.jq.find('.articleTarget').fadeIn('slow');
+                    });
+                });
+            };
+
+            for (var c = articlesArr.length - 1; c >= 0; c--) {
+                if(isElementInViewport(articlesArr[c].jq) && !articlesArr[c].isLoaded){
+                    loader(articlesArr[c]);
                 }
             };
         }
     }
 
-    var handler = onVisibilityChange();
+    var handler = onVisibilityChange(),
+        pagesFixed = true;
 
     $(window).on('DOMContentLoaded load resize scroll', function(e){
-        // $.throttle( 50, handler() );
+        console.log($(window).scrollTop());
         handler();
         $.throttle(0, function(){
             console.log('scrolled');
         });
-        // onVisibilityChange(el, callback);
-        // onVisibilityChange();
+        if($(window).scrollTop() >= 942 && pagesFixed == true){
+            pagesFixed = false;
+            $('#pages').addClass('relative');
+        } else if($(window).scrollTop() < 942 && pagesFixed == false) {
+            pagesFixed = true;
+            $('#pages').removeClass('relative');
+
+        }
     });
+
+    articlesArr[0].isLoaded = true;
+    articlesArr[0].jq.load( articlesArr[0].link + " .articleContents" );
 }); 
